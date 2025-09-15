ff1e78269ec0bc121327483629c4eb609ec8f229
Yehor Huzev <98352189+ttekit@users.noreply.github.com>
2025-09-15 10:41:49 +0300

    v1.0.0

--- main.ts
+++ main.ts
@@ -1,81 +1,42 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Editor,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+	ButtonComponent,
+	TFile
+} from 'obsidian';
+import {forEach} from "builtin-modules";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+export interface TagMapping {
+	mainTag: string;
+	additionalTag: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+interface ColorViaTagSettings {
+	tagMappings: TagMapping[];
+	DEFAULT_COLOR_TAG_NAME: string;
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const DEFAULT_SETTINGS: ColorViaTagSettings = {
+	tagMappings: [],
+	DEFAULT_COLOR_TAG_NAME: "color"
+}
+
+
+export interface MyPluginSettings {
+}
+
+export default class ColorViaTag extends Plugin {
+	settings: ColorViaTagSettings;
 
 	async onload() {
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -89,28 +50,85 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+
+	async getFilesWithTag(tag: string): Promise<TFile[]> {
+		const notes: TFile[] = [];
+
+		this.app.vault.getMarkdownFiles().forEach((file: TFile) => {
+			const cache = this.app.metadataCache.getFileCache(file);
+			if (!cache) return;
+
+			if (cache.tags) {
+				for (const t of cache.tags) {
+					if (t.tag === `#${tag}`) {
+						notes.push(file);
+						return; // already matched, skip further checks
+					}
+				}
+			}
+
+			if (cache.frontmatter && cache.frontmatter["tags"]) {
+				const frontTags = Array.isArray(cache.frontmatter["tags"])
+					? cache.frontmatter["tags"]
+					: [cache.frontmatter["tags"]];
+				if (frontTags.includes(tag)) {
+					notes.push(file);
+				}
+			}
+		});
+
+		return notes;
+	}
+
+	async addProperty(file: TFile, key: string, value: any): Promise<"added" | "updated"> {
+		let status: "added" | "updated" = "added";
+		await this.app.fileManager.processFrontMatter(file, (frontmatter) => {
+			if (frontmatter[key] !== undefined) {
+				status = "updated";
+			}
+			frontmatter[key] = value;
+		});
+		return status;
+	}
+
+	async removeProperty(file: TFile, key: string) {
+		await this.app.fileManager.processFrontMatter(file, (frontmatter) => {
+			if (frontmatter[key] !== undefined) {
+				delete frontmatter[key];
+			}
+		});
+	}
+
+
+	public applyTagMappings() {
+		this.settings.tagMappings.forEach(element => {
+			console.log(element);
+			this.getFilesWithTag(element.mainTag).then(files_array => {
+				files_array.forEach((file) => {
+					this.addProperty(file, this.settings.DEFAULT_COLOR_TAG_NAME, element.additionalTag)
+					console.log(file);
+
+				})
+			})
+		})
+	}
+
+	public removeTagMappings(id: number) {
+		this.getFilesWithTag(this.settings.tagMappings[id].mainTag).then(files_array => {
+			files_array.forEach((file) => {
+				this.removeProperty(file, this.settings.DEFAULT_COLOR_TAG_NAME)
+			})
+		})
+	}
+
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: ColorViaTag;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: ColorViaTag) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,15 +138,70 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
+		// Table container
+		const table = containerEl.createEl("div", {cls: "tag-mapping-table"});
+
+		// Render each mapping row
+		this.plugin.settings.tagMappings.forEach((mapping, index) => {
+			this.renderMappingRow(table, mapping, index);
+		});
+
+		// Add new row button
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.addButton((btn) => {
+				btn.setButtonText("Add Row")
+					.setCta()
+					.onClick(() => {
+						this.plugin.settings.tagMappings.push({mainTag: "", additionalTag: ""});
+						this.plugin.saveSettings();
+						this.display();
+					});
+			});
+
+		// Confirm button
+		new Setting(containerEl)
+			.addButton((btn: ButtonComponent) => {
+				btn.setButtonText("Confirm")
+					.setCta()
+					.onClick(async () => {
+						await this.plugin.applyTagMappings();
+					});
+			});
+
+	}
+
+	private renderMappingRow(table: HTMLElement, mapping: TagMapping, index: number) {
+		const row = table.createDiv({cls: "tag-mapping-row"});
+
+		// Main tag input
+		const mainInput = row.createEl("input", {
+			type: "text",
+			value: mapping.mainTag,
+			placeholder: "Main tag"
+		});
+		mainInput.oninput = (e: any) => {
+			this.plugin.settings.tagMappings[index].mainTag = e.target.value;
+			this.plugin.saveSettings();
+		};
+
+		// Additional tag input
+		const additionalInput = row.createEl("input", {
+			type: "text",
+			value: mapping.additionalTag,
+			placeholder: "Additional tag"
+		});
+		additionalInput.oninput = (e: any) => {
+			this.plugin.settings.tagMappings[index].additionalTag = e.target.value;
+			this.plugin.saveSettings();
+		};
+
+		// Delete button
+		const delBtn = row.createEl("button", {text: "✕"});
+		delBtn.onclick = () => {
+			this.plugin.removeTagMappings(index);
+			this.plugin.settings.tagMappings.splice(index, 1);
+			this.plugin.saveSettings();
+			this.display();
+		};
 	}
 }
